0817cc4976963b7f15880972b814fffb8fbbbb36
amakzy <ahmadamakxy128@gmail.com>
2023-05-24 16:01:31 -0700

    test

--- utils.c
+++ utils.c
@@ -1,30 +1,31 @@
 #include "shell.h"
 
 /**
- * Auth: Emma Udeji
- * 		 Pericles Adjovi
  *
  * Description:
  * the extended functions for main.c
  */
 
 /* Internal command hash table */
-static const struct {
-    char *command_name;
-    void (*func)(char **command);
-} internal_commands[] = {
-    {"env", env},
-    {"exit", quit},
-    {NULL, NULL}
+static const struct
+{
+	char *command_name;
+	void (*func)(char **command);
+}
+internal_commands[] ={
+	{"env", env},
+	{"exit", quit},
+	{NULL, NULL}
 };
 
 /* Hash function for internal command lookup */
-static unsigned int hash_command(const char *command) {
-    unsigned int hash = 5381;
-    int c;
-    while ((c = *command++))
-        hash = ((hash << 5) + hash) + c;
-    return hash;
+static unsigned int hash_command(const char *command)
+{
+	unsigned int hash = 5381;
+	int c;
+	while ((c = *command++))
+		hash = ((hash << 5) + hash) + c;
+	return (hash);
 }
 
 /** parse_command - determines the type of the command
@@ -37,27 +38,31 @@ static unsigned int hash_command(const char *command) {
  *		 PATH_COMMAND (3) represents commands found in the PATH like ls
  *		 INVALID_COMMAND (-1) represents invalid commands
  */
-int parse_command(char *command) {
-    int i;
-    char *path = NULL;
+int parse_command(char *command)
+{
+	int i;
+	char *path = NULL;
 
-    for (i = 0; command[i] != '\0'; i++) {
-        if (command[i] == '/')
-            return EXTERNAL_COMMAND;
-    }
+	for (i = 0; command[i] != '\0'; i++)
+	{
+		if (command[i] == '/')
+			return (EXTERNAL_COMMAND);
+	}
 
-    for (i = 0; internal_commands[i].command_name != NULL; i++) {
-        if (_strcmp(command, internal_commands[i].command_name) == 0)
-            return INTERNAL_COMMAND;
-    }
+	for (i = 0; internal_commands[i].command_name != NULL; i++)
+	{
+		if (_strcmp(command, internal_commands[i].command_name) == 0)
+			return (INTERNAL_COMMAND);
+	}
 
-    path = check_path(command);
-    if (path != NULL) {
-        free(path);
-        return PATH_COMMAND;
-    }
+	path = check_path(command);
+	if (path != NULL)
+	{
+		free(path);
+		return (PATH_COMMAND);
+	}
 
-    return INVALID_COMMAND;
+	return (INVALID_COMMAND);
 }
 
 /**
@@ -67,40 +72,45 @@ int parse_command(char *command) {
  *
  * Return: void
  */
-void execute_command(char **tokenized_command, int command_type) {
-    void (*func)(char **command);
-    char *path = NULL;
+void execute_command(char **tokenized_command, int command_type)
+{
+	void (*func)(char **command);
+	char *path = NULL;
 
-    switch (command_type) {
-        case EXTERNAL_COMMAND:
-            if (execve(tokenized_command[0], tokenized_command, NULL) == -1) {
-                perror(_getenv("PWD"));
-                exit(2);
-            }
-            break;
-        case PATH_COMMAND:
-            path = check_path(tokenized_command[0]);
-            if (path != NULL) {
-                if (execve(path, tokenized_command, NULL) == -1) {
-                    perror(_getenv("PWD"));
-                    exit(2);
-                }
-                free(path);
-            }
-            break;
-        case INTERNAL_COMMAND:
-            func = get_func(tokenized_command[0]);
-            if (func != NULL)
-                func(tokenized_command);
-            break;
-        case INVALID_COMMAND:
-            print(shell_name, STDERR_FILENO);
-            print(": 1: ", STDERR_FILENO);
-            print(tokenized_command[0], STDERR_FILENO);
-            print(": not found\n", STDERR_FILENO);
-            status = 127;
-            break;
-    }
+	switch (command_type)
+	{
+		case EXTERNAL_COMMAND:
+			if (execve(tokenized_command[0], tokenized_command, NULL) == -1)
+			{
+				perror(_getenv("PWD"));
+				exit(2);
+			}
+			break;
+		case PATH_COMMAND:
+			path = check_path(tokenized_command[0]);
+			if (path != NULL)
+			{
+				if (execve(path, tokenized_command, NULL) == -1)
+				{
+					perror(_getenv("PWD"));
+					exit(2);
+				}
+				free(path);
+			}
+			break;
+		case INTERNAL_COMMAND:
+			func = get_func(tokenized_command[0]);
+			if (func != NULL)
+				func(tokenized_command);
+			break;
+		case INVALID_COMMAND:
+			print(shell_name, STDERR_FILENO);
+			print(": 1: ", STDERR_FILENO);
+			print(tokenized_command[0], STDERR_FILENO);
+			print(": not found\n", STDERR_FILENO);
+			status = 127;
+			break;
+	}
 }
 
 /**
@@ -109,32 +119,36 @@ void execute_command(char **tokenized_command, int command_type) {
  *
  * Return: path where the command is found in, NULL if not found
  */
-char *check_path(char *command) {
-    char *path = _getenv("PATH");
-    char *token, *path_cpy;
-    char *full_path = NULL;
+char *check_path(char *command)
+{
+	char *path = _getenv("PATH");
+	char *token, *path_cpy;
+	char *full_path = NULL;
 
-    if (path == NULL || _strlen(path) == 0)
-        return NULL;
+	if (path == NULL || _strlen(path) == 0)
+		return (NULL);
 
-    path_cpy = strdup(path);
-    token = strtok(path_cpy, ":");
-    while (token != NULL) {
-        full_path = malloc(strlen(token) + strlen(command) + 2); // +2 for '/' and null terminator
-        if (full_path == NULL) {
-            perror("malloc");
-            exit(2);
-        }
-        sprintf(full_path, "%s/%s", token, command);
-        if (access(full_path, F_OK) == 0)
-            break;
+	path_cpy = strdup(path);
+	token = strtok(path_cpy, ":");
 
-        free(full_path);
-        token = strtok(NULL, ":");
-    }
-    free(path_cpy);
+	while (token != NULL)
+	{
+		full_path = malloc(strlen(token) + strlen(command) + 2);
+		if (full_path == NULL)
+		{
+			perror("malloc");
+			exit(2);
+		}
+		sprintf(full_path, "%s/%s", token, command);
+		if (access(full_path, F_OK) == 0)
+			break;
 
-    return full_path;
+		free(full_path);
+		token = strtok(NULL, ":");
+	}
+	free(path_cpy);
+
+	return (full_path);
 }
 
 /**
@@ -143,18 +157,21 @@ char *check_path(char *command) {
  *
  * Return: pointer to the proper function, or null on fail
  */
-void (*get_func(char *command))(char **) {
-    unsigned int hash = hash_command(command);
-    int i;
+void (*get_func(char *command))(char **)
+{
+	unsigned int hash = hash_command(command);
+	int i;
 
-    for (i = 0; internal_commands[i].command_name != NULL; i++) {
-        if (hash == hash_command(internal_commands[i].command_name)) {
-            if (_strcmp(command, internal_commands[i].command_name) == 0)
-                return internal_commands[i].func;
-        }
-    }
+	for (i = 0; internal_commands[i].command_name != NULL; i++)
+	{
+		if (hash == hash_command(internal_commands[i].command_name))
+		{
+			if (_strcmp(command, internal_commands[i].command_name) == 0)
+				return (internal_commands[i].func);
+		}
+	}
 
-    return NULL;
+	return (NULL);
 }
 
 /**
@@ -163,21 +180,25 @@ void (*get_func(char *command))(char **) {
  *
  * Return: the value of the variable as a string
  */
-char *_getenv(char *name) {
-    char **my_environ;
-    char *pair_ptr;
-    char *name_cpy;
 
-    for (my_environ = environ; *my_environ != NULL; my_environ++) {
-        for (pair_ptr = *my_environ, name_cpy = name;
-             *pair_ptr == *name_cpy; pair_ptr++, name_cpy++) {
-            if (*pair_ptr == '=')
-                break;
-        }
-        if ((*pair_ptr == '=') && (*name_cpy == '\0'))
-            return pair_ptr + 1;
-    }
+char *_getenv(char *name)
+{
+	char **my_environ;
+	char *pair_ptr;
+	char *name_cpy;
 
-    return NULL;
+	for (my_environ = environ; *my_environ != NULL; my_environ++)
+	{
+		for (pair_ptr = *my_environ, name_cpy = name;
+				*pair_ptr == *name_cpy; pair_ptr++, name_cpy++)
+		{
+			if (*pair_ptr == '=')
+				break;
+		}
+		if ((*pair_ptr == '=') && (*name_cpy == '\0'))
+			return (pair_ptr + 1);
+	}
+
+	return (NULL);
 }
 
